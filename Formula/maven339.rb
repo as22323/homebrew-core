73653f06c008a54292523b2ed6abfdc0bc7d001d
as22323 <a.k.shah@ieee.org>
2017-10-09 23:41:23 -0400

    Update maven339.rb

--- Formula/maven339.rb
+++ Formula/maven339.rb
@@ -5,6 +5,7 @@ class Maven < Formula
   stable do
     url "https://www.apache.org/dyn/closer.cgi?path=maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.tar.gz"
     mirror "https://archive.apache.org/dist/maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.tar.gz"
+    sha256 "beb91419245395bd69a4a6edad5ca3ec1a8b64e41457672dc687c173a495f034"
   end
 
   bottle :unneeded
